4e3f948e7bcc038d5740174a6c68cee52b837846
IT Admin <admin@itadmin.com.mx>
2020-02-27 16:20:17 -0500

    [FIX] Add acccount report ext module

---
+++ account_reports_ext/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Account reports Operating Unit",
+    "version": "12.0.1.0.0",
+    "author": "IT Admin",
+    "license": "LGPL-3",
+    "website": "",
+    "category": "",
+    "depends": [
+        "account_operating_unit",
+        "account_reports",
+    ],
+    "data": [
+        
+    ],
+    'installable': True,
+}
